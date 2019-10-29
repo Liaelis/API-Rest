9b11459ac51df78476a8fa1c929ec6705f07c2f9
Elis Regina <er.paisagista@gmail.com>
2019-10-29 19:55:09 -0300

    primeiro commit
    cadastro funcionando

---
+++ leaf_store/src/test/java/elis/com/leaf_store/LeafStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package elis.com.leaf_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
